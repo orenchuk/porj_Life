fb5fd4c14572a048c2c7a9f4684f4de94665ee4f
Леша Фурман <alexeyfurman@macbook-pro-alexey.local>
2016-05-19 16:30:34 +0200

    Initial Commit

---
+++ LifeSimulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LifeSimulator
+//
+//  Created by Alexey Furman on 19.05.16.
+//  Copyright © 2016 Alexey Furman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
